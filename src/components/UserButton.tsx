12cc75e75201e7e129e0b43d7c34d0859c926bcc
Nirupam Changmai <73378284+nirupamc@users.noreply.github.com>
2024-11-03 23:06:11 +0530

    dark mode applied

--- src/components/UserButton.tsx
+++ src/components/UserButton.tsx
@@ -1,44 +1,89 @@
 "use client";
 
-import { useSession } from "@/app/(main)/SessionProvider";
-import { DropdownMenu, DropdownMenuTrigger,DropdownMenuContent,DropdownMenuLabel,DropdownMenuSeparator, DropdownMenuItem } from "./ui/dropdown-menu";
-import UserAvatar from "./UserAvatar";
-import Link from "next/link";
-import { LogOutIcon, UserIcon } from "lucide-react";
 import { logout } from "@/app/(auth)/actions";
+import { useSession } from "@/app/(main)/SessionProvider";
 import { cn } from "@/lib/utils";
+// import { useQueryClient } from "@tanstack/react-query";
+import { Check, LogOutIcon, Monitor, Moon, Sun, UserIcon } from "lucide-react";
+import { useTheme } from "next-themes";
+import Link from "next/link";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import UserAvatar from "./UserAvatar";
 
-
- interface UserButtonProps {
-    className?: string;
+interface UserButtonProps {
+  className?: string;
 }
-export default function UserButton({className}:UserButtonProps){    
-    const { user } = useSession();
-    return <DropdownMenu>
-        <DropdownMenuTrigger asChild>
+
+export default function UserButton({ className }: UserButtonProps) {
+  const { user } = useSession();
+
+  const { theme, setTheme } = useTheme();
+
+//   const queryClient = useQueryClient();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
         <button className={cn("flex-none rounded-full", className)}>
           <UserAvatar avatarUrl={user.avatarUrl} size={40} />
         </button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-            <DropdownMenuLabel>
-                Logged in as @{user.username}
-            </DropdownMenuLabel>
-            <DropdownMenuSeparator/>
-            <Link href={`/users/${user.username}`}>
-            <DropdownMenuItem>
-                <UserIcon className="mr-2 side-4"/>
-                Profile
-            </DropdownMenuItem>
-            </Link>
-            <DropdownMenuSeparator/>
-            <DropdownMenuItem 
-            onClick={()=>{
-                logout()
-            }}>
-                <LogOutIcon className="mr-2 size-4"/>
-                Logout
-            </DropdownMenuItem>
-        </DropdownMenuContent>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>Logged in as @{user.username}</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <Link href={`/users/${user.username}`}>
+          <DropdownMenuItem>
+            <UserIcon className="mr-2 size-4" />
+            Profile
+          </DropdownMenuItem>
+        </Link>
+        <DropdownMenuSub>
+          <DropdownMenuSubTrigger>
+            <Monitor className="mr-2 size-4" />
+            Theme
+          </DropdownMenuSubTrigger>
+          <DropdownMenuPortal>
+            <DropdownMenuSubContent>
+              <DropdownMenuItem onClick={() => setTheme("system")}>
+                <Monitor className="mr-2 size-4" />
+                System default
+                {theme === "system" && <Check className="ms-2 size-4" />}
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setTheme("light")}>
+                <Sun className="mr-2 size-4" />
+                Light
+                {theme === "light" && <Check className="ms-2 size-4" />}
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setTheme("dark")}>
+                <Moon className="mr-2 size-4" />
+                Dark
+                {theme === "dark" && <Check className="ms-2 size-4" />}
+              </DropdownMenuItem>
+            </DropdownMenuSubContent>
+          </DropdownMenuPortal>
+        </DropdownMenuSub>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem
+          onClick={() => {
+            // queryClient.clear();
+            logout();
+          }}
+        >
+          <LogOutIcon className="mr-2 size-4" />
+          Logout
+        </DropdownMenuItem>
+      </DropdownMenuContent>
     </DropdownMenu>
+  );
 }
