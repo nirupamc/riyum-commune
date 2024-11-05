611c7582f536924de1b2c99aadac8d62ffe1e496
Nirupam Changmai <73378284+nirupamc@users.noreply.github.com>
2024-11-05 21:10:24 +0530

    trending topics are there and the users to follow is also visible Y_Y

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,6 +2,7 @@
 
 import PostEditor from "@/components/posts/editor/PostEditor";
 import Post from "@/components/posts/Post";
+import TrendsSidebar from "@/components/TrendsSidebar";
 import prisma from "@/lib/prisma";
 import {postDataInclude} from "@/lib/types"
 
@@ -12,7 +13,7 @@ export default async function Home() {
     orderBy: {createdAt: "desc"}
   })
 return (
-    <main className="w-full min-w-0 gap-5">
+    <main className="w-full min-w-0 flex gap-5">
       <div className="w-full min-w-0 space-y-5">
       <PostEditor />
       
@@ -20,6 +21,7 @@ return (
           <Post key={post.id} post={post} />
         ))}
       </div>
+      <TrendsSidebar/>
     </main>
   );
 }
